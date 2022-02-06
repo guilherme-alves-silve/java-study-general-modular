e7f3f270e6b24c0a65afde05e2ce89aa9e448627
Guilherme Alves Silveira <guilherme.alves.silve@gmail.com>
2022-02-06 01:40:34 -0300

    Implement queue using stacks

---
+++ src/br.com.datastructure/classes/br/com/guilhermealvessilve/certification/study/datastructure/stack/interview/MainQueueWithStack.java
@@ -0,0 +1,26 @@
+package br.com.guilhermealvessilve.certification.study.datastructure.stack.interview;
+
+/**
+ *
+ * @author Alves
+ */
+public class MainQueueWithStack {
+    
+    /**
+     * Stack with queue overview
+     * Interview Question #2
+     * The aim is to design a queue abstract data type with the help of stacks.
+     * Reference:
+     *  https://www.udemy.com/course/algorithms-and-data-structures/learn/lecture/11582440#overview
+     */
+    public static void main(String[] args) {
+        var queue = new QueueWithStack<Integer>();
+        queue.enqueue(10, 18, 30);
+        System.out.println("queue.pop: " + queue.dequeue());
+        queue.enqueue(35);
+        int size = queue.size();
+        for (int i = 0; i < size; i++) {
+            System.out.println("queue.pop: " + queue.dequeue());
+        }
+    }
+}
---
+++ src/br.com.datastructure/classes/br/com/guilhermealvessilve/certification/study/datastructure/stack/interview/QueueWithStack.java
@@ -0,0 +1,45 @@
+package br.com.guilhermealvessilve.certification.study.datastructure.stack.interview;
+
+import br.com.guilhermealvessilve.certification.study.datastructure.stack.implementation.Stack;
+
+/**
+ *
+ * @author Alves
+ */
+public class QueueWithStack<E extends Comparable<E>> {
+    
+    private final Stack<E> enqueueStack;
+    private final Stack<E> dequeueStack;
+
+    public QueueWithStack() {
+        this.enqueueStack = new Stack<>();
+        this.dequeueStack = new Stack<>();
+    }
+    
+    public void enqueue(E data) {
+        enqueueStack.push(data);
+    }
+    
+    public void enqueue(E... elements) {
+        enqueueStack.push(elements);
+    }
+    
+    public E dequeue() {
+        
+        if (dequeueStack.isEmpty()) {
+            while (!enqueueStack.isEmpty()) {
+                dequeueStack.push(enqueueStack.pop());
+            }
+        }
+        
+        return dequeueStack.pop();
+    }
+    
+    public int size() {
+        return enqueueStack.size() + dequeueStack.size();
+    }
+    
+    public boolean isEmpty() {
+        return enqueueStack.isEmpty() && dequeueStack.isEmpty();
+    }
+}
